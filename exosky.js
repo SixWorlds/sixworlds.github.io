aefb0b7256ff93d06a4ebca357cf29d34ea14596
AdamKuziemski <kuziemski.a@gmail.com>
2024-10-05 19:18:18 +0200

    Extract skybox to its own file, add controls and planets

---
+++ exosky.js
@@ -0,0 +1,26 @@
+const planetPicker = document.getElementById('planet-picker');
+// planetPicker.attributes.setNamedItem('disabled');
+let planets = null;
+
+async function loadPlanets() {
+  const planetsResponse = await fetch('https://sixworlds.github.io/assets/planets.json');
+  planets = await planetsResponse.json();
+  
+  Object.keys(planets)
+    .sort((a,b) => a.localeCompare(b))
+    .forEach((name) => {
+      const option = document.createElement('option');
+      option.value = name;
+      option.innerText = name;
+      planetPicker.appendChild(option);
+    });
+  
+  loading.style.display = 'none';
+}
+
+planetPicker.addEventListener('change', (e) => {
+  const selectedPlanet = e.target.value;
+  console.log('look I changed', planets[selectedPlanet]);
+});
+
+loadPlanets().then();

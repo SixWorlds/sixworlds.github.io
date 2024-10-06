8a03eadcb78e2dac3f058dec6baf4cd1de7b3b89
AdamKuziemski <kuziemski.a@gmail.com>
2024-10-06 13:48:46 +0200

    Add skymaps

--- exosky.js
+++ exosky.js
@@ -1,6 +1,7 @@
 const planetPicker = document.getElementById('planet-picker');
 // planetPicker.attributes.setNamedItem('disabled');
 let planets = null;
+currentPlanet = '';
 
 async function loadPlanets() {
   const planetsResponse = await fetch('https://sixworlds.github.io/assets/planets_demo.json');
@@ -16,13 +17,23 @@ async function loadPlanets() {
     });
   
   switchSkyBox(Object.keys(planets)[0], 'skybox');
+  currentPlanet = Object.keys(planets)[0];
   loading.style.display = 'none';
   document.getElementById('drag').style.display = 'block';
 }
 
 planetPicker.addEventListener('change', (e) => {
   const selectedPlanet = e.target.value;
+  currentPlanet = selectedPlanet;
   switchSkyBox(selectedPlanet, 'skybox');
+});
+
+document.getElementById('skymap-n', () => {
+  window.open(`https://sixworlds.github.io/assets/${currentPlanet}/skymap_n.png`, 'blank');
+});
+
+document.getElementById('skymap-s', () => {
+  window.open(`https://sixworlds.github.io/assets/${currentPlanet}/skymap_s.png`, 'blank');
 });
 
 loadPlanets().then();
